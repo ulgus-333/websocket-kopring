605d1c0a7e88db64addd56e71288a9ece914a3e6
jhk <wlgus120332@gmail.com>
2026-01-11 20:48:57 +0900

    init: project init

---
+++ src/test/kotlin/com/practice/socket/WebsocketKopringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practice.socket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsocketKopringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
