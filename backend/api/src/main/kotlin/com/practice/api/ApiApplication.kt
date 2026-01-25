5e1366fc7b864f3c8c3ad8080d38ab9be80b2da9
jhk <wlgus120332@gmail.com>
2026-01-25 21:01:00 +0900

    chore: build.gradle 정리 및 의존성 재정의

--- backend/api/src/main/kotlin/com/practice/api/ApiApplication.kt
+++ backend/api/src/main/kotlin/com/practice/api/ApiApplication.kt
@@ -1,13 +1,9 @@
 package com.practice.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(scanBasePackages = ["com.practice.common", "com.practice.api", "com.practice.infra"])
-@EnableJpaRepositories(basePackages = ["com.practice.common.repository"])
-@EntityScan(basePackages = ["com.practice.common.domain.entity"])
 class ApiApplication
 
 fun main(args: Array<String>) {
--- backend/infra/build.gradle.kts
+++ backend/infra/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.springframework.boot")
     id("org.jetbrains.kotlin.plugin.spring")
+    id("java-library")
 }
 
 dependencies {
